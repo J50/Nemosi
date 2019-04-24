d2e9e795cf3ec9a722aca0bd35facda2d5ff1c45
Joe 50 <joefifty50@gmail.com>
2019-04-24 16:15:39 -0400

    bug fixes and built json file to send to database

--- app/src/main/java/com/cmu/Nemosi/DailyTest.java
+++ app/src/main/java/com/cmu/Nemosi/DailyTest.java
@@ -2,29 +2,46 @@ package com.cmu.Nemosi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.app.PendingIntent.getActivity;
+
 public class DailyTest extends AppCompatActivity {
 
+    ArrayList<TextView> questions;
+    ArrayList<Spinner> spinners;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_daily_test);
+        this.setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
 
 //        TextView myTextView = findViewById(R.id.myTextView);
 //        myTextView.setText("Welcome to Daily Check-in");
 
         // initialize arrays for each question-input
-        ArrayList<Spinner> spinners = new ArrayList<Spinner>();
-        ArrayList<TextView> questions = new ArrayList<TextView>();
+        spinners = new ArrayList<Spinner>();
         ArrayList<LinearLayout> layouts = new ArrayList<LinearLayout>();
+        questions = new ArrayList<TextView>();
         ArrayList<String> question_strings = new ArrayList<String>();
 
         // set each layout for each question-input
@@ -68,7 +85,7 @@ public class DailyTest extends AppCompatActivity {
         yesNo.add("Yes");
         yesNo.add("No");
         ArrayAdapter<String> secondAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, severity);
+                android.R.layout.simple_spinner_item, yesNo);
         secondAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
         spinners.get(0).setAdapter(firstAdapter);
@@ -84,6 +101,49 @@ public class DailyTest extends AppCompatActivity {
         spinners.get(10).setAdapter(secondAdapter);
         spinners.get(11).setAdapter(secondAdapter);
         spinners.get(12).setAdapter(secondAdapter);
+
+        Button sendTest = new Button(this);
+        sendTest.setText("Send Test");
+        sendTest.setLayoutParams(new LinearLayout.LayoutParams(ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        sendTest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                JSONArray jsonArray = new JSONArray();
+                for(int i = 0; i <= 12; i++) {
+                    JSONObject entry = new JSONObject();
+                    try {
+                        entry.put("patient_name", i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        entry.put("test_results", i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        entry.put("patient_id", i);
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        entry.put("scaled_rating1", questions.get(i).toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    try {
+                        entry.put("scaled_rating2", spinners.get(i).toString());
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                    jsonArray.put(entry);
+                }
+                System.out.println("jsonString: "+jsonArray);
+                Toast.makeText(DailyTest.this, "Sent results to server!", Toast.LENGTH_LONG).show();
+            }
+        });
+        bigLayout.addView(sendTest);
 
 
     }
